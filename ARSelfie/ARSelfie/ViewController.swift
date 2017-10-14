1e4a23830fc782487239809d545239d1e14263e6
Alex Curylo <alex@alexcurylo.com>
2017-10-14 11:55:51 +0700

    Fill out delegate methods

--- ARSelfie/ARSelfie/ViewController.swift
+++ ARSelfie/ARSelfie/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SpriteKit
 import ARKit
 
-class ViewController: UIViewController, ARSKViewDelegate {
+internal class ViewController: UIViewController {
     
     @IBOutlet var sceneView: ARSKView!
     
@@ -22,8 +22,12 @@ class ViewController: UIViewController, ARSKViewDelegate {
         
         // Show statistics such as fps and node count
         sceneView.showsFPS = true
+        sceneView.showsDrawCount = true
         sceneView.showsNodeCount = true
-        
+        sceneView.showsQuadCount = true
+        sceneView.showsPhysics = true
+        sceneView.showsFields = true
+
         // Load the SKScene from 'Scene.sks'
         if let scene = SKScene(fileNamed: "Scene") {
             sceneView.presentScene(scene)
@@ -51,9 +55,45 @@ class ViewController: UIViewController, ARSKViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+}
+
+// MARK: - ARSessionObserver
+extension ViewController: ARSessionObserver {
+
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+    }
+
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        // This is called when the camera’s tracking state has changed.
+    }
+
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    }
     
-    // MARK: - ARSKViewDelegate
-    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+
+    func session(_ session: ARSession, didOutputAudioSampleBuffer audioSampleBuffer: CMSampleBuffer) {
+        // This is called when the session outputs a new audio sample buffer.
+    }
+}
+
+// MARK: - SKViewDelegate
+extension ViewController: SKViewDelegate {
+
+    public func view(_ view: SKView, shouldRenderAtTime time: TimeInterval) -> Bool {
+        // Allows the client to dynamically control the render rate.
+        return true
+    }
+}
+
+// MARK: - ARSKViewDelegate
+extension ViewController: ARSKViewDelegate {
+
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
         // Create and configure a node for the anchor added to the view's session.
         let labelNode = SKLabelNode(text: "🤡")
@@ -62,18 +102,19 @@ class ViewController: UIViewController, ARSKViewDelegate {
         return labelNode;
     }
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+    func view(_ view: ARSKView, didAdd node: SKNode, for anchor: ARAnchor) {
+        // Called when a new node has been mapped to the given anchor.
+    }
+
+    func view(_ view: ARSKView, willUpdate node: SKNode, for anchor: ARAnchor) {
+        // Called when a node will be updated with data from the given anchor.
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    func view(_ view: ARSKView, didUpdate node: SKNode, for anchor: ARAnchor) {
+        // Called when a node has been updated with data from the given anchor.
     }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+
+    func view(_ view: ARSKView, didRemove node: SKNode, for anchor: ARAnchor) {
+        // Called when a mapped node has been removed from the scene graph for the given anchor.
     }
 }
