3ad1a773c8b24135168084c4376affdf2a546af1
Alex Curylo <alex@alexcurylo.com>
2017-10-27 13:22:27 +0300

    Move anchor back

--- ARSelfie/ARSelfie/Scene.swift
+++ ARSelfie/ARSelfie/Scene.swift
@@ -27,9 +27,9 @@ class Scene: SKScene {
         // Create anchor using the camera's current position
         if let currentFrame = sceneView.session.currentFrame {
             
-            // Create a transform with a translation of 0.2 meters in front of the camera
+            // Create a transform with a translation of 0.8 meters in front of the camera
             var translation = matrix_identity_float4x4
-            translation.columns.3.z = -0.2
+            translation.columns.3.z = -0.8
             let transform = simd_mul(currentFrame.camera.transform, translation)
             
             // Add a new anchor to the session
